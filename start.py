ce7a6c1ed78ef358268b55487f502aba7e7083f1
Christian Stöveken <christian.stoeveken@gmail.com>
2014-08-09 18:16:14 +0200

    fucking motors working again ... raspberry pi dreck

--- start.py
+++ start.py
@@ -15,49 +15,85 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_OFF)    
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
 
-#instantiate 3 global variables for the encoders
-#front right = fr
-#front left = fl
-#back = b
+#Enable motor pins (fr, fl, b) GPIO.LOW == 0
+GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)
+
+#motor direction pins (PWM [full_cw ... off ... full_ccw] [0 ... 50 ... 100]
+GPIO.setup(15, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+
+#todo: put in motor class /drive class
+#setup PWM 50Hz?
+motor_fl = GPIO.PWM(15, 50)
+motor_fr = GPIO.PWM(16, 50)
+motor_b = GPIO.PWM(18, 50)
+
+GPIO.output(7, GPIO.HIGH)
+GPIO.output(11, GPIO.HIGH)
+GPIO.output(13, GPIO.HIGH)
+
+motor_fl.start(35)
+motor_fr.start(35)
+motor_b.start(35)
 
 
+
+#Encoder class
 class Encoder:
-    def __init__(self):
-        self.enc_fr = 0
-        self.enc_fl = 0
-        self.enc_b = 0
+    def __init__(self, encGPIO):
+        #counter variable
+        self.enc_count = 0
+        #encoder GPIO (maybe we don't even need it)
+        self.encGPIO = encGPIO
 
-    def fl(self):
-        self.enc_fl += 1
-        print "fl" + str(self.enc_fl)
-    def fr(self):
-        self.enc_fr += 1
-        print "fr" + str(self.enc_fr)
-    def b(self):
-        self.enc_b += 1
-        print "b" + str(self.enc_b)
+        #which direction (1=cw/2=ccw)
+        self.direction = 1
 
-encoder = Encoder()
+    def count(self):
+        self.enc_count += self.direction
+        print "pin: " + str(self.encGPIO) + " count: " + str(self.enc_count)
 
-options = {19: encoder.fr,
-           21: encoder.fl,
-           23: encoder.b,
+    def change_dir(self, direction):
+        if direction == "cw":
+            self.direction = 1
+        elif direction == "ccw":
+            self.direction = -1
+        else:
+            print "what direction is that supposed to be?"
+
+
+# instantiate the three encoder objects
+encoder_fr = Encoder(19)
+encoder_fl = Encoder(21)
+encoder_b = Encoder(23)
+
+encoder_fr.change_dir("ccw")
+
+#encoder gpio lookup table
+encoder_count = {19: encoder_fr.count,
+           21: encoder_fl.count,
+           23: encoder_b.count,
 }
 
+#the callback function for the encoders
 def encoder_callback(enc_num):
 
-    print "pin change detected" + str(enc_num)
-    options[enc_num]()
+    print "pin change detected: " + str(enc_num)
+    #python "switch case" (lookup) to change the right counter variable
+    encoder_count[enc_num]()
 
 
 
 
 #append the encoder event to it's callback function
-GPIO.add_event_detect(19, GPIO.BOTH, callback=encoder_callback, bouncetime=3)
-GPIO.add_event_detect(21, GPIO.BOTH, callback=encoder_callback, bouncetime=3)
-GPIO.add_event_detect(23, GPIO.BOTH, callback=encoder_callback, bouncetime=3)
+GPIO.add_event_detect(19, GPIO.BOTH, callback=encoder_callback, bouncetime=2)
+GPIO.add_event_detect(21, GPIO.BOTH, callback=encoder_callback, bouncetime=2)
+GPIO.add_event_detect(23, GPIO.BOTH, callback=encoder_callback, bouncetime=2)
 
 
 
@@ -73,4 +109,8 @@ try:
 
 except KeyboardInterrupt:
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+    motor_fr.stop()
+    motor_fb.stop()
+    motor_b.stop()
+
 GPIO.cleanup()           # clean up GPIO on normal exit
