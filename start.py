de952c5993f4f15b87552056d3f837d52d395f5c
Christian Stöveken <christian.stoeveken@gmail.com>
2014-08-11 00:02:42 +0200

    it is alive ... dirty as it comes ... but first success controlling the bot with a website and wlan connection

--- start.py
+++ start.py
@@ -2,6 +2,78 @@ import time
 import sys
 import RPi.GPIO as GPIO
 
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+class LightsHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        print "ola: " + self.path
+        if ("ledon" in self.path):
+            self.statusled_on()
+        elif ("ledoff" in self.path):
+            self.statusled_off()
+        elif ("move" in self.path):
+            if("move0" in self.path):
+                mc.move(0,25)
+                time.sleep(1)
+                mc.move(0,0)
+
+            elif("move1" in self.path):
+                mc.move(60, 25)
+                time.sleep(1)
+                mc.move(60, 0)
+
+            elif("move2" in self.path):
+                mc.move(120, 25)
+                time.sleep(1)
+                mc.move(120, 0)
+
+            elif("move3" in self.path):
+                mc.move(180, 25)
+                time.sleep(1)
+                mc.move(180, 0)
+
+            elif("move4" in self.path):
+                mc.move(240, 25)
+                time.sleep(1)
+                mc.move(240, 0)
+
+            elif("move5" in self.path):
+                mc.move(300, 25)
+                time.sleep(1)
+                mc.move(300, 0)
+
+            elif("mover" in self.path):
+                mc.rotate(-1,25)
+                time.sleep(1)
+                mc.rotate(-1,0)
+
+            elif("movel" in self.path):
+                mc.rotate(1,25)
+                time.sleep(1)
+                mc.rotate(1,0)
+
+
+        self.wfile.write("""<!DOCTYPE html><body>Hello</br>
+                      <a href="move0">0</a></br>
+                      <a href="move1">1</a></br>
+                      <a href="move2">2</a></br>
+                      <a href="move3">3</a></br>
+                      <a href="move4">4</a></br>
+<a href="move5">5</a></br>
+<a href="movel">l</a></br>
+<a href="mover">r</a></br>
+
+                     World !</body>""")
+        return
+
+    def statusled_on(self):
+        GPIO.output(22, True)
+        print "becoming active"
+    def statusled_off(self):
+        print "going offline"
+        GPIO.output(22, False)
+
 #pin number = gpio number! otherwise use BCM
 GPIO.setmode(GPIO.BOARD)
 
@@ -155,20 +227,38 @@ class MotionController():
 
 mc = MotionController()
 
+HandlerClass = LightsHandler
+ServerClass=BaseHTTPServer.HTTPServer
+Protocol="HTTP/1.0"
+
+server_address= ("", 8080)
+
+HandlerClass.protocol_version = Protocol
+httpd = ServerClass(server_address, HandlerClass)
+
+sa = httpd.socket.getsockname()
+print "serving"
+
+GPIO.setup(22, GPIO.OUT)
+
+httpd.serve_forever()
+
+
+
 # 'bouncetime=300' includes the bounce control written into interrupts2a.py
 
 try:
     while 1:
         print '.'
 
-        for i in range (-1, 2, 2):
-            mc.rotate(i,25)
-            time.sleep(3)
+#        for i in range (-1, 2, 2):
+ #           mc.rotate(i,25)
+  #          time.sleep(3)
 
-        for i in range(0,301,60):
-            mc.move(i, 25)
-            time.sleep(3)
-        time.sleep(10)
+   #     for i in range(0,301,60):
+    #        mc.move(i, 25)
+     #       time.sleep(3)
+        time.sleep(1)
 
 except KeyboardInterrupt:
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit
