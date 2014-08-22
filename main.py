aa539d176c4c9144f1e261a0e9d1704c564e46f5
Waynix <der@waynix.org>
2014-08-22 19:35:50 +0200

    added main startup file

---
+++ main.py
@@ -0,0 +1,22 @@
+from start import mc
+import triweb
+
+
+
+print("Initializing motor framework")
+
+
+
+
+print("Motor Framework is initialized")
+print("Initializing motor framework")
+
+def callback(data):
+    print(repr(data))
+triweb.startup()
+
+
+print("Initializing motor framework")
+
+
+
